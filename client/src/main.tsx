9ff3508b33159ea599fd7867223aa64f28db805b
Jair Quintana Carrión <jairquintana50@gmail.com>
2026-05-25 19:47:28 +0200

    chore: add React Query Devtools for enhanced debugging

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,12 +4,14 @@ import './style/main.css'
 import { router } from './router/routes'
 import { RouterProvider } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
